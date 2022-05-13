55096eabc97ed78e18f9da5270b586def7a24f01
cyw7515@naver.com <cyw7515@naver.com>
2022-05-13 21:08:09 +0900

    Fix: 종료되지 않는 오류, socket bind 오류 수정

--- pa2/P1/receiver.py
+++ pa2/P1/receiver.py
@@ -9,7 +9,7 @@ except IndexError:
   print("python receiver.py <result file name> <log file name>")
   sys.exit(1)
 recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-recv_sock.bind(('127.0.0.1', 10090))
+recv_sock.bind(('', 10090))
 
 try:
   file = open(res_filename, 'wb')
@@ -17,6 +17,10 @@ except Exception:
   print("file not found")
   sys.exit(1)
 
+print("RDT Start")
 message, addr = recv_sock.recvfrom(1024)
 file.write(message)
+
 file.close()
+recv_sock.close()
+print("RDT end")
