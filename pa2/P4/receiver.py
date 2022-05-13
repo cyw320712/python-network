8288e4963f1cd199d8a4af594577fd16184fff1f
cyw7515@naver.com <cyw7515@naver.com>
2022-05-13 12:46:34 +0900

    Fix: 마지막 윈도우에서 에러가 나면 receiver에서 처리하지 못하고 바로 종료하는 문제 해결

--- pa2/P4/receiver.py
+++ pa2/P4/receiver.py
@@ -1,5 +1,6 @@
 import struct
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import socket
 from checksum import calculate_checksum
@@ -27,59 +28,74 @@ if __name__ == '__main__':
   src_port = 10090
   dst_port = 10091
   window_size = 0
-
+  sequence_size = 0
+  packet_len = 9999
+  pos = 0
+  window = []
   print("RDT Start")
-  while True:
-    message, addr = sock.recvfrom(1048)
-    
+  while pos < packet_len:
+    message, addr = sock.recvfrom(1024)
+
     recv_header = struct.unpack('!6H', message[:12])
     src_port = recv_header[0]
     dst_port = recv_header[1]
     recv_seq = recv_header[2]
     window_size = recv_header[3]
-    recv_isFin = recv_header[4]
+    packet_len = recv_header[4]
     recv_checksum = recv_header[5]
-    recv_header = struct.pack('!6H', src_port, dst_port, recv_seq, window_size, recv_isFin, 0)
+    recv_header = struct.pack('!6H', src_port, dst_port, recv_seq, window_size, packet_len, 0)
     recv_content = message[12:]
-    if recv_isFin == 1:
-      break
 
     calculated = calculate_checksum(recv_header + recv_content)
     STATUS = 0
     if calculated == recv_checksum:
       # 문제 없으면
+      if window_size == 0:
+        window_size = 0
+      if sequence_size == 0:
+        sequence_size = window_size * 2 + 1
+      if len(window) != packet_len:
+        for _ in range(packet_len):
+          window.append(0)
       if expecting_seq == recv_seq:
         STATUS = 1
       else:
         log_handler.writeAck(recv_seq, log_handler.WRONG_SEQ_NUM)
-        STATUS = 0
     else:
       # Corrupted 됐다면
       log_handler.writeAck(expecting_seq, log_handler.CORRUPTED)
     
     if STATUS:
       # 기다리던 sequence number가 왔으며, 문제가 없다면
-      segment = ("ACK"+str(expecting_seq)).encode()
+      segment = ("ACK").encode()
       header = struct.pack('!5H', src_port, dst_port, expecting_seq, STATUS, 0)
       checksum = calculate_checksum(header+segment)
       header = struct.pack('!5H', src_port, dst_port, expecting_seq, STATUS, checksum)
 
       sender.sendto_bytes(header + segment, addr)
-      log_handler.writeAck(expecting_seq, log_handler.SEND_ACK)
-
-      file.write(recv_content)
+      if window[pos] == 0:
+        window[pos] = 1
+        log_handler.writeAck(expecting_seq, log_handler.SEND_ACK)
+        file.write(recv_content)
+        pos+=1
+      else:
+        log_handler.writeAck(expecting_seq, log_handler.SEND_ACK_AGAIN)
       expecting_seq += 1
-      expecting_seq %= window_size
+      expecting_seq %= sequence_size
     else:
       # 기다리던 sequence number가 아니거나, corrupt 됐으면
-      segment = ("ACK"+str(expecting_seq)).encode()
-      header = struct.pack('!5H', src_port, dst_port, expecting_seq, STATUS, 0)
+      if expecting_seq == 0:
+        negative_seq = 11
+      else:
+        negative_seq = expecting_seq -1
+      segment = ("ACK").encode()
+      header = struct.pack('!5H', src_port, dst_port, negative_seq, STATUS, 0)
       checksum = calculate_checksum(header+segment)
-      header = struct.pack('!5H', src_port, dst_port, expecting_seq, STATUS, checksum)
+      header = struct.pack('!5H', src_port, dst_port, negative_seq, STATUS, checksum)
 
       sender.sendto(header + segment, addr)
-      log_handler.writeAck(expecting_seq, log_handler.SEND_ACK_AGAIN)
-      
+      log_handler.writeAck(negative_seq, log_handler.SEND_ACK_AGAIN)
+    
   
   sock.close()
   file.close()
